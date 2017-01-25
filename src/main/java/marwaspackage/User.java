924c421b106624335312116b21b7252547c71336
marwamc <marwac@live.com>
2017-01-25 10:51:09 -0500

    marwas first commit. rudimentary user class.

---
+++ src/main/java/marwaspackage/User.java
@@ -0,0 +1,24 @@
+package marwaspackage;
+
+/**
+ * Created by mkulima on 1/25/17.
+ */
+public class User {
+    private String name;
+    private double balance;
+
+    User(String name, double balance){
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+}
