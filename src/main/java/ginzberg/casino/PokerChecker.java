5bd5e7244268d960c764c0eb37504b30c636b17f
William Mattern <williammattern@zipcoders-MacBook-Pro-3.local>
2017-01-26 14:25:48 -0500

    poker checker running and tested

--- src/main/java/ginzberg/casino/PokerChecker.java
+++ src/main/java/ginzberg/casino/PokerChecker.java
@@ -6,14 +6,8 @@ import java.util.*;
  * Created by williammattern on 1/25/17.
  */
 public class PokerChecker {
-    CardDeck deck = new CardDeck();
-    HashMap<Integer, Card> playerHand = new HashMap<Integer, Card>(5);
-    {
 
-        for (Integer i = 0; i < 5; i++) {
-            playerHand.put(i, deck.getNextCard());
-        }
-    }
+
 
     public boolean flushChecker(HashMap<Integer, Card> hand){
         Suits[] suitArray = new Suits[5];
@@ -45,8 +39,13 @@ public class PokerChecker {
         } else { return false; }
     }
 
-    public boolean royalChecker(){
-        return false;
+    public boolean royalChecker(HashMap<Integer, Card> hand){
+        int[] rankOrdinalArray = convertToRankOrdinalArray(hand);
+        for (int i: rankOrdinalArray){
+            if (i > 0 && i < 9){
+                return false;
+            }
+        } return true;
     }
 
     public int[] convertToRankOrdinalArray(HashMap<Integer, Card> hand){
@@ -68,7 +67,7 @@ public class PokerChecker {
     }
 
     public int countOfMatchesFromLeft(int[] rankOrdinalArray){
-        int countOfMatchesFromLeft = 0;
+        int countOfMatchesFromLeft = 1;
         for (int i = 0; i < rankOrdinalArray.length; i++){
             if (rankOrdinalArray[i] == rankOrdinalArray[i + 1]){
                 countOfMatchesFromLeft++;
@@ -78,8 +77,8 @@ public class PokerChecker {
     }
 
     public int countOfMatchesFromRight(int[] rankOrdinalArray){
-        int countOfMatchesFromRight = 0;
-        for (int i = rankOrdinalArray.length; i < 0; i--){
+        int countOfMatchesFromRight = 1;
+        for (int i = rankOrdinalArray.length - 1; i > 0; i--){
             if (rankOrdinalArray[i] == rankOrdinalArray[i-1]){
                 countOfMatchesFromRight++;
             } else {return countOfMatchesFromRight;}
@@ -88,20 +87,104 @@ public class PokerChecker {
     }
 
     public boolean fullHouseChecker(HashMap<Integer, Card> hand){
+        int[] rankOrdinalArray = convertToRankOrdinalArray(hand);
+        Arrays.sort(rankOrdinalArray);
+        int countFromLeft = countOfMatchesFromLeft(rankOrdinalArray);
+        int countFromRight = countOfMatchesFromRight(rankOrdinalArray);
+        if((countFromLeft == 3 && countFromRight == 2) || (countFromLeft == 2 && countFromRight == 3)){
+            return true;
+        } else {return false;}
+    }
+
+    public int countOfMatchesFromPosition2(int[] rankOrdinalArray){
+        int countOfMatchesFromPosition2 = 1;
+        for (int i = 1; i < rankOrdinalArray.length; i++){
+            if (rankOrdinalArray[i] == rankOrdinalArray[i+1]){
+                countOfMatchesFromPosition2++;
+            } else {return countOfMatchesFromPosition2;}
+        }
+        return countOfMatchesFromPosition2;
+    }
+
+    public boolean tripsChecker(HashMap<Integer, Card> hand){
+        int[] rankOrdinalArray = convertToRankOrdinalArray(hand);
+        Arrays.sort(rankOrdinalArray);
+        int countFromLeft, countFromRight, countFromPosition2;
+        countFromLeft = countOfMatchesFromLeft(rankOrdinalArray);
+        countFromRight = countOfMatchesFromRight(rankOrdinalArray);
+        countFromPosition2 = countOfMatchesFromPosition2(rankOrdinalArray);
+        if (countFromLeft == 3 || countFromPosition2 == 3 || countFromRight == 3){
+            return true;
+        } else
         return false;
     }
 
-    public String getWinConditionThatIsMet(){
-        if (flushChecker(playerHand) && straightChecker(playerHand) && royalChecker()){
+
+    public boolean checkForPairAroundPosition2(int[] rankOrdinalArray){
+        if (rankOrdinalArray[1] == rankOrdinalArray[0] || rankOrdinalArray[1] == rankOrdinalArray[2]){
+            return true;
+        } else {return false;}
+    }
+
+    public boolean checkForPairAroundPosition4(int[] rankOrdinalArray){
+        if (rankOrdinalArray[3] == rankOrdinalArray[2] || rankOrdinalArray[3] == rankOrdinalArray[4]){
+            return true;
+        } else {return false;}
+    }
+
+    public boolean checkForJacksOrBetterPairAroundPosition2(int[] rankOrdinalArray){
+        if (rankOrdinalArray[1] == rankOrdinalArray[0] && (rankOrdinalArray[1] == 0 || rankOrdinalArray[1] > 9)){
+            return true;
+        } else if (rankOrdinalArray[1] == rankOrdinalArray[2] && (rankOrdinalArray[1] == 0 || rankOrdinalArray[1] > 9)) {
+            return true;
+        } else { return false;}
+    }
+
+    public boolean checkForJacksOrBetterPairAroundPosition4(int[] rankOrdinalArray){
+        if (rankOrdinalArray[3] == rankOrdinalArray[2] && (rankOrdinalArray[3] == 0 || rankOrdinalArray[3] > 9)){
+            return true;
+        } else if (rankOrdinalArray[3] == rankOrdinalArray[4] && (rankOrdinalArray[3] == 0 || rankOrdinalArray[3] > 9)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean twoPairChecker(HashMap<Integer, Card> hand){
+        int[] rankOrdinalArray = convertToRankOrdinalArray(hand);
+        Arrays.sort(rankOrdinalArray);
+        if(checkForPairAroundPosition2(rankOrdinalArray) && checkForPairAroundPosition4(rankOrdinalArray)){
+            return true;
+        } else {return false;}
+    }
+
+    public boolean jacksOrBetterChecker(HashMap<Integer, Card> hand){
+        int[] rankOrdinalArray = convertToRankOrdinalArray(hand);
+        Arrays.sort(rankOrdinalArray);
+        if(checkForJacksOrBetterPairAroundPosition2(rankOrdinalArray) || checkForJacksOrBetterPairAroundPosition4(rankOrdinalArray)){
+            return true;
+        } else {return false;}
+    }
+
+    public String getWinConditionThatIsMet(HashMap<Integer, Card> playerHand){
+        if (flushChecker(playerHand) && straightChecker(playerHand) && royalChecker(playerHand)){
             return "royalFlush";
         } else if (flushChecker(playerHand) && straightChecker(playerHand) ){
             return "straightFlush";
         } else if (fourOfAKindChecker(playerHand)){
             return "fourOfAKind";
         } else if (fullHouseChecker(playerHand)) {
-
-        }
-
-        return "lostHand";
+            return "fullHouse";
+        } else if (flushChecker(playerHand)){
+            return "flush";
+        } else if (straightChecker(playerHand)){
+            return "straight";
+        } else if (tripsChecker(playerHand)){
+            return "threeOfAKind";
+        } else if (twoPairChecker(playerHand)){
+            return "twoPair";
+        } else if (jacksOrBetterChecker(playerHand)){
+            return "jacksOrBetter";
+        } else { return "lostHand";}
     }
 }
--- src/test/java/ginzberg/casino/PokerCheckerTest.java
+++ src/test/java/ginzberg/casino/PokerCheckerTest.java
@@ -12,19 +12,37 @@ import static org.junit.Assert.*;
  */
 public class PokerCheckerTest {
     Card aceOfSpades = new Card(Ranks.ACE,Suits.SPADES);
-    Card twoOfClubs = new Card(Ranks.TWO, Suits.CLUBS);
+    Card aceOfClubs = new Card(Ranks.ACE,Suits.CLUBS);
     Card aceOfDiamonds = new Card(Ranks.ACE,Suits.DIAMONDS);
+    Card twoOfClubs = new Card(Ranks.TWO, Suits.CLUBS);
     Card threeOfHearts = new Card(Ranks.THREE, Suits.HEARTS);
     Card kingOfHearts = new Card(Ranks.KING,Suits.HEARTS);
+    Card kingOfSpades = new Card(Ranks.KING,Suits.SPADES);
+    Card kingOfDiamonds = new Card(Ranks.KING,Suits.DIAMONDS);
+    Card kingOfClubs = new Card(Ranks.KING,Suits.CLUBS);
+    Card fourOfDiamonds = new Card(Ranks.FOUR, Suits.DIAMONDS);
+    Card fiveOfSpades = new Card(Ranks.FIVE, Suits.SPADES);
+    Card fiveOfDiamonds = new Card(Ranks.FIVE,Suits.DIAMONDS);
+    Card threeOfDiamonds = new Card(Ranks.THREE, Suits.DIAMONDS);
+    Card sixOfDiamonds = new Card(Ranks.SIX,Suits.DIAMONDS);
+    Card tenOfSpades = new Card(Ranks.TEN, Suits.SPADES);
+    Card jackOfSpades = new Card(Ranks.JACK, Suits.SPADES);
+    Card queenOfSpades = new Card(Ranks.QUEEN, Suits.SPADES);
+    Card nineOfSpades = new Card(Ranks.NINE, Suits.SPADES);
 
-    @Test
-    public void seeCardsInHand(){
-        PokerChecker pokerChecker = new PokerChecker();
-        for (int i =0; i < 5; i++){
-            System.out.println(pokerChecker.playerHand.get(i).getRank().ordinal());
+    public HashMap<Integer, Card> loadPlayerHandWithFiveCards(Card cardOne, Card cardTwo, Card cardThree, Card cardFour, Card cardFive){
+        CardDeck deck = new CardDeck();
+        deck.cards.add(0,cardOne);
+        deck.cards.add(1,cardTwo);
+        deck.cards.add(2,cardThree);
+        deck.cards.add(3,cardFour);
+        deck.cards.add(4,cardFive);
+        HashMap<Integer, Card> playerHand = new HashMap<Integer, Card>(5);
+        for (Integer i = 0; i < 5; i++) {
+            playerHand.put(i, deck.getNextCard());
         }
+        return playerHand;
     }
-
 
     @Test
     public void flushCheckerTrueTest(){
@@ -35,7 +53,7 @@ public class PokerCheckerTest {
             playerHand.put(i, deck.getNextCard());
         }
         boolean actual = pokerChecker.flushChecker(playerHand);
-        System.out.println(actual);
+        assertTrue(actual);
     }
 
     @Test
@@ -52,7 +70,7 @@ public class PokerCheckerTest {
             playerHand.put(i, deck.getNextCard());
         }
         boolean actual = pokerChecker.flushChecker(playerHand);
-        System.out.println(actual);
+        assertFalse(actual);
     }
 
     @Test
@@ -64,7 +82,7 @@ public class PokerCheckerTest {
             playerHand.put(i, deck.getNextCard());
         }
         boolean actual = pokerChecker.straightChecker(playerHand);
-        System.out.println(actual);
+        assertTrue(actual);
     }
 
     @Test
@@ -81,7 +99,7 @@ public class PokerCheckerTest {
             playerHand.put(i, deck.getNextCard());
         }
         boolean actual = pokerChecker.straightChecker(playerHand);
-        System.out.println(actual);
+        assertFalse(actual);
     }
 
     @Test
@@ -115,5 +133,251 @@ public class PokerCheckerTest {
         assertArrayEquals(expected,actual);
     }
 
+    @Test
+    public void countOfMatchesFromLeftTest(){
+        int[] ordinalArray = {0,0,12,12,12};
+        PokerChecker pokerChecker = new PokerChecker();
+        int actual = pokerChecker.countOfMatchesFromLeft(ordinalArray);
+        int expected = 2;
+        assertEquals(expected, actual,0.0);
+    }
 
+    @Test
+    public void countOfMatchesFromRightTest(){
+        int[] ordinalArray = {0,0,12,12,12};
+        PokerChecker pokerChecker = new PokerChecker();
+        int actual = pokerChecker.countOfMatchesFromRight(ordinalArray);
+        int expected = 3;
+        assertEquals(expected, actual,0.0);
+    }
+
+    @Test
+    public void countOfMatchesFromPosition2(){
+        int[] ordinalArray = {0,0,0,12,12};
+        PokerChecker pokerChecker = new PokerChecker();
+        int actual = pokerChecker.countOfMatchesFromPosition2(ordinalArray);
+        int expected = 2;
+        assertEquals(expected, actual,0.0);
+    }
+
+    @Test
+    public void fullHouseCheckerTest2pair3pair(){
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfSpades,aceOfSpades,kingOfClubs,kingOfDiamonds,aceOfClubs);
+        PokerChecker pokerChecker = new PokerChecker();
+        boolean actual = pokerChecker.fullHouseChecker(playerHand);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void fullHouseCheckerTest3pair2pair(){
+        CardDeck deck = new CardDeck();
+        deck.cards.add(0,aceOfSpades);
+        deck.cards.add(1,aceOfClubs);
+        deck.cards.add(2,aceOfDiamonds);
+        deck.cards.add(3,kingOfDiamonds);
+        deck.cards.add(4,kingOfSpades);
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = new HashMap<Integer, Card>(5);
+        for (Integer i = 0; i < 5; i++) {
+            playerHand.put(i, deck.getNextCard());
+        }
+        boolean actual = pokerChecker.fullHouseChecker(playerHand);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void tripsCheckerTest3pair(){
+        CardDeck deck = new CardDeck();
+        deck.cards.add(0,aceOfSpades);
+        deck.cards.add(1,aceOfClubs);
+        deck.cards.add(2,aceOfDiamonds);
+        deck.cards.add(3,threeOfHearts);
+        deck.cards.add(4,kingOfSpades);
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = new HashMap<Integer, Card>(5);
+        for (Integer i = 0; i < 5; i++) {
+            playerHand.put(i, deck.getNextCard());
+        }
+        boolean actual = pokerChecker.tripsChecker(playerHand);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void twoPairCheckerTest(){
+        CardDeck deck = new CardDeck();
+        deck.cards.add(0,kingOfClubs);
+        deck.cards.add(1,aceOfClubs);
+        deck.cards.add(2,aceOfDiamonds);
+        deck.cards.add(3,threeOfHearts);
+        deck.cards.add(4,kingOfSpades);
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = new HashMap<Integer, Card>(5);
+        for (Integer i = 0; i < 5; i++) {
+            playerHand.put(i, deck.getNextCard());
+        }
+        boolean actual = pokerChecker.twoPairChecker(playerHand);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void twoPairCheckerTestForFalse(){
+        CardDeck deck = new CardDeck();
+        deck.cards.add(0,kingOfClubs);
+        deck.cards.add(1,twoOfClubs);
+        deck.cards.add(2,aceOfDiamonds);
+        deck.cards.add(3,threeOfHearts);
+        deck.cards.add(4,kingOfSpades);
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = new HashMap<Integer, Card>(5);
+        for (Integer i = 0; i < 5; i++) {
+            playerHand.put(i, deck.getNextCard());
+        }
+        boolean actual = pokerChecker.twoPairChecker(playerHand);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void jacksOrBetterCheckerTestingForFalse() {
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfClubs,twoOfClubs,aceOfDiamonds,threeOfHearts,fiveOfSpades);
+        PokerChecker pokerChecker = new PokerChecker();
+        boolean actual = pokerChecker.jacksOrBetterChecker(playerHand);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void jacksOrBetterCheckerTestingForLowPair() {
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfClubs,twoOfClubs,aceOfDiamonds,threeOfHearts,threeOfDiamonds);
+        boolean actual = pokerChecker.jacksOrBetterChecker(playerHand);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void jacksOrBetterCheckerTestingForTrue() {
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfClubs, kingOfDiamonds, aceOfDiamonds, threeOfHearts, fiveOfDiamonds);
+        boolean actual = pokerChecker.jacksOrBetterChecker(playerHand);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void jacksOrBetterCheckerTestingForAces() {
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(aceOfSpades,threeOfDiamonds,fiveOfDiamonds,fourOfDiamonds,aceOfClubs);
+        boolean actual = pokerChecker.jacksOrBetterChecker(playerHand);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void royalCheckerFalseTest(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(aceOfSpades,threeOfDiamonds,fiveOfDiamonds,fourOfDiamonds,aceOfClubs);
+        boolean actual = pokerChecker.royalChecker(playerHand);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void royalCheckerTrueTest(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(tenOfSpades,jackOfSpades,queenOfSpades,kingOfSpades,aceOfSpades);
+        boolean actual = pokerChecker.royalChecker(playerHand);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void decisionTreeTestRoyal(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(tenOfSpades,jackOfSpades,queenOfSpades,kingOfSpades,aceOfSpades);
+        String expected = "royalFlush", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestSF(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(tenOfSpades,jackOfSpades,queenOfSpades,kingOfSpades,nineOfSpades);
+        String expected = "straightFlush", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestFourOfAKind(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfSpades,kingOfDiamonds,kingOfClubs,aceOfClubs,kingOfHearts);
+        String expected = "fourOfAKind", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestFullHouse(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfSpades,kingOfDiamonds,kingOfClubs,aceOfClubs,aceOfSpades);
+        String expected = "fullHouse", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestFlush(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfSpades,tenOfSpades,fiveOfSpades,aceOfSpades,nineOfSpades);
+        String expected = "flush", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestLowStraight(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(aceOfSpades,twoOfClubs,threeOfDiamonds,fourOfDiamonds,fiveOfSpades);
+        String expected = "straight", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestMiddleStraight(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(twoOfClubs,threeOfDiamonds,fourOfDiamonds,fiveOfSpades,sixOfDiamonds);
+        String expected = "straight", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestHighStraight(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(tenOfSpades,jackOfSpades,queenOfSpades,kingOfDiamonds,aceOfSpades);
+        String expected = "straight", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestThreeOfAKind(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfSpades,kingOfDiamonds,kingOfClubs,fourOfDiamonds,threeOfDiamonds);
+        String expected = "threeOfAKind", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestTwoPair(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(fiveOfDiamonds,fiveOfSpades,threeOfDiamonds,threeOfHearts,kingOfDiamonds);
+        String expected = "twoPair", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void decisionTreeTestJacksOrBetter(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfSpades,kingOfDiamonds,threeOfDiamonds,fiveOfDiamonds,nineOfSpades);
+        String expected = "jacksOrBetter", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void decisionTreeTestLostHand(){
+        PokerChecker pokerChecker = new PokerChecker();
+        HashMap<Integer, Card> playerHand = loadPlayerHandWithFiveCards(kingOfSpades,jackOfSpades,fourOfDiamonds,threeOfDiamonds,fiveOfSpades);
+        String expected = "lostHand", actual = pokerChecker.getWinConditionThatIsMet(playerHand);
+        assertEquals(expected,actual);
+    }
 }
