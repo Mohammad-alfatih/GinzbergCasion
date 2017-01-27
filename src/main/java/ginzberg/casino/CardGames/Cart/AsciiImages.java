5aea89f5e6b01c822a9f001726332a2f64fb3adf
marwamc <marwac@live.com>
2017-01-27 12:15:15 -0500

    started working on cli welcome screens

--- src/main/java/ginzberg/casino/CardGames/Cart/AsciiImages.java
+++ src/main/java/ginzberg/casino/CardGames/Cart/AsciiImages.java
@@ -9,9 +9,9 @@ import java.io.IOException;
  * Created by mkulima on 1/27/17.
  */
 public class AsciiImages {
-    String rootLoc = "//Users/anthonypajerowski/Casino/GinzbergCasion/src/main/java/ginzberg/casino/CardGames/Cart/";
-    //String rootLoc = "/home/mkulima/dev/labs/GinzbergCasion/src/main/java/ginzberg/casino/CardGames/Cart/";
-//    String rootLoc = "/Users/williammattern/dev/GinzbergCasion/src/main/java/ginzberg/casino/CardGames/Cart/";
+    //String rootLoc = "//Users/anthonypajerowski/Casino/GinzbergCasion/src/main/java/ginzberg/casino/CardGames/Cart/";
+    String rootLoc = "/home/mkulima/dev/labs/GinzbergCasion/src/main/java/ginzberg/casino/CardGames/Cart/";
+    //String rootLoc = "/Users/williammattern/dev/GinzbergCasion/src/main/java/ginzberg/casino/CardGames/Cart/";
 
     public void showJoker() {
         BufferedReader br = null;
@@ -23,6 +23,33 @@ public class AsciiImages {
         String line = null;
         try {
             while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showCasino() {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(rootLoc+"AsciiCasinoName.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String line = null;
+        try {
+            while ((line = br.readLine()) != null) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println(line);
             }
         } catch (IOException e) {
