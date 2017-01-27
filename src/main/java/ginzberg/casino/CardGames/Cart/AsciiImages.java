a8732855bc3012e601a7799ef6ff63f1cb1b78e9
marwamc <marwac@live.com>
2017-01-27 13:49:27 -0500

    added auto filepath for ascii images.

--- src/main/java/ginzberg/casino/CardGames/Cart/AsciiImages.java
+++ src/main/java/ginzberg/casino/CardGames/Cart/AsciiImages.java
@@ -9,8 +9,9 @@ import java.io.IOException;
  * Created by mkulima on 1/27/17.
  */
 public class AsciiImages {
+    DetermineFilePath determineFilePath = new DetermineFilePath();
     //String rootLoc = "//Users/anthonypajerowski/Casino/GinzbergCasion/src/main/java/ginzberg/casino/CardGames/Cart/";
-    String rootLoc = "/home/mkulima/dev/labs/GinzbergCasion/src/main/java/ginzberg/casino/CardGames/Cart/";
+    String rootLoc = determineFilePath.whereAmI()+"/src/main/java/ginzberg/casino/CardGames/Cart/";
     //String rootLoc = "/Users/williammattern/dev/GinzbergCasion/src/main/java/ginzberg/casino/CardGames/Cart/";
 
     public void showJoker() {
