a381f119e38b8a16f2c47ca1e7405f9a4d64a7d5
Toonces1980 <anthony.pajerowski@gmail.com>
2017-01-27 12:13:15 -0500

    deleted additional poker game payout methods from pokerChecker

--- src/main/java/ginzberg/casino/CardGames/Poker/PokerInputParser.java
+++ src/main/java/ginzberg/casino/CardGames/Poker/PokerInputParser.java
@@ -5,21 +5,22 @@ package ginzberg.casino.CardGames.Poker;
  * Created by anthonypajerowski on 1/25/17.
  */
 public class PokerInputParser {
-
-    public int[] parseUserInput(String input){
-        if (input.equals("none")){
-            int[] results = new int[0];
-            return results;
-        }
-        String[] items = input.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll("\\s", "").split(",");
-        int[] results = new int[items.length];
-        for (int i = 0; i < items.length; i++) {
-            try {
-                results[i] = Integer.parseInt(items[i]);
-            } catch (NumberFormatException nfe) {
-                System.out.println(nfe);
+    int[] results;
+    public int[] parseUserInput(String input) {
+            if (input.equals("none")) {
+                results = new int[0];
+                return results;
             }
-        }
+            String[] items = input.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll("\\s", "").split(",");
+            results = new int[items.length];
+            for (int i = 0; i < items.length; i++) {
+                try {
+                    results[i] = Integer.parseInt(items[i]);
+                } catch (NumberFormatException nfe) {
+                    System.out.println(nfe);
+                }
+            }
+
         return results;
     }
 }
